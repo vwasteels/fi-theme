d6a76b46c13122bd3faa72efc08b850e897029b8
Vincent Wasteels <vincent.wasteels@gmail.com>
2017-05-17 10:44:36 +0200

    move ACF fields to PHP

--- functions.php
+++ functions.php
@@ -3,12 +3,13 @@
 define('AUTOSAVE_INTERVAL', 300 ); // seconds
 define('WP_POST_REVISIONS', false );
 
+
 /*
  * Include php
  */
 
-
 require_once(TEMPLATEPATH.'/includes/acf.php');
+require_once(TEMPLATEPATH.'/includes/fields.php');
 require_once(TEMPLATEPATH.'/includes/tools.php');
 require_once(TEMPLATEPATH.'/includes/register.php');
 ?>
